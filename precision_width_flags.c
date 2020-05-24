333fea272945e8d40ccd214009c413bfa1c2dcef
Matheus Ferreira de Oliveira Costa <44984872+mferoc@users.noreply.github.com>
2020-05-24 12:27:00 -0300

    Update precision_width_flags.c comments
    Update precision_width_flags.c comments

--- precision_width_flags.c
+++ precision_width_flags.c
@@ -42,7 +42,7 @@ int main()
 	*	WIDTH is a spacer (ident) to format how output is displayed
 	*	%WidthSpecifier
 	*	10 is the minimum width but in my tests this is variable (look the second printf below)
-		//// Width minimum is the greater then specifier ////
+	*	//// Width minimum is the greater then specifier ////
 	*/
 	printf("%10s has %10.1lf feet\n", name, feet);
 	printf("%9s has %4.1lf feet\n", name, feet);
@@ -51,9 +51,9 @@ int main()
 	printf("%8d\n", 42);
 	printf("%7d\n", 42);
 	/*
-		FLAGS
-		the flags works in accord of your functions, see below
-		the flag has preference and overlaps others
+	*	FLAGS
+	*	the flags works in accord of your functions, see below
+	*	the flag has preference and overlaps others
 	*/
 	printf("%-10dX\n", 42);
 	printf("%09dX\n", 42);
